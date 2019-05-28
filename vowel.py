20810fddfc4ad06a3b1757ba788efd64cd91888a
S Dinesh <42296714+officiallydinesh@users.noreply.github.com>
2019-05-28 09:49:25 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,10 @@
+#sddinesh
+ch = input()
+
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
+       or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
+    print("Vowel")
+elif(ch.isdidit()):
+   print("Invalid")
+else:
+   print("Consonant")
