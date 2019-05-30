a78683cb9d308208833259927abaf7b3bdcf8ded
S Dinesh <42296714+officiallydinesh@users.noreply.github.com>
2019-05-30 17:16:17 +0530

    Create evenrange.py

---
+++ evenrange.py
@@ -0,0 +1,8 @@
+a,b=input().split()
+for i in range(int(a),int(b)+1):
+    if i==int(a):
+        continue
+    elif i==int(b):
+        continue
+    elif i % 2 == 0: 
+        print(i, end = " ")  
