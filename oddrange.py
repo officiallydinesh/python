eada2ccd4b6a8ee53b862747d867dbd3410e4339
S Dinesh <42296714+officiallydinesh@users.noreply.github.com>
2019-05-30 17:13:45 +0530

    Update oddrange.py

--- oddrange.py
+++ oddrange.py
@@ -1,5 +1,9 @@
 a,b=input().split()
-for i in range(int(a),int(b)+1,):
-    if i % 2 != 0: 
-        print(i, end = " ") 
+for i in range(int(a),int(b)+1):
+    if i==int(a):
+        continue
+    elif i==int(b):
+        continue
+    elif i % 2 != 0: 
+        print(i, end = " ")
        
